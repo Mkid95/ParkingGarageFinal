b8195a7eade1eacf550f09b9061c59c6020e58a6
Mkid95 <matthewbespinoza@gmail.com>
2019-04-29 16:33:33 -0400

    laptop push test

--- app/src/main/java/com/example/parkinggaragefinal/MainActivity.java
+++ app/src/main/java/com/example/parkinggaragefinal/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //test push from laptop
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
