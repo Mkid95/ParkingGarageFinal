1d440056cbe353b6257ba1ddbcfc1a3ae3c02abe
Mkid95 <matthewbespinoza@gmail.com>
2021-10-19 15:40:56 -0400

    Screenshot added

--- app/src/main/java/com/example/parkinggaragefinal/MainActivity.java
+++ app/src/main/java/com/example/parkinggaragefinal/MainActivity.java
@@ -16,7 +16,7 @@ import java.io.InputStreamReader;
 import java.util.Calendar;
 
 
-//todo fix time, fix license, make admin first.
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
